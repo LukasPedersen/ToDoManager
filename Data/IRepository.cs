8869a58b8417c478d743006115bb9883d75ecfd1
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2021-02-22 16:03:46 +0100

    Add project files.

---
+++ Data/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using ToDoManager.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoManager.Data
+{
+    public interface IRepository
+    {
+        // Hent alle personer
+        List<Person> GetAll();
+        // Hent person ud fra Id
+        Person GetItemById(int? id);
+        // Indsæt en ny person i listen
+        void Insert(Person person);
+        // Opdatere en eksisterende person i listen
+        void Update(Person person);
+        // Fjern en person fra listen
+        void Remove(int id);
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoManager.Data;
+using ToDoManager.Models;
+using System.Threading.Tasks;
+
+namespace ToDoManager.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRepository _context;
+
+        public IndexModel(IRepository context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get; set; }
+
+        public void OnGet()
+        {
+            Person = _context.GetAll();
+        }
+    }
+}
